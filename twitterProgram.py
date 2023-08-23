e66c2da262985f3ced5eec39ca4868f2b9b63561
nullThink <legoking43@gmail.com>
2023-08-22 22:44:37 -0700

    [TwitterBot] Fixed issue where bot wasn't setting random time to send message.

--- twitterProgram.py
+++ twitterProgram.py
@@ -15,22 +15,25 @@ def api():
 def sendWormTweet(client, api, message=""):
     wormImage = api.media_upload("IMG_3297.JPG")
     client.create_tweet(text=message, media_ids=[wormImage.media_id])
+    print("Tweeted!")
 
 def setRandTime(client, apiObj):
     hourInt = random.randint(0, 23)
     minuteInt = random.randint(0, 59)
 
-    scheduler.remove_job("worm_message")
-    scheduler.add_job(sendWormTweet(client, apiObj), trigger=CronTrigger(hour=hourInt, minute=minuteInt),id="worm_message")
-
-    print(hourInt)
-    print(minuteInt)
+    for job in scheduler.get_jobs():
+        if(job.id == "worm_message"):
+            scheduler.remove_job("worm_message")
+    
+    def sendTweet():
+        sendWormTweet(client, apiObj, "Who up?")
+    
+    scheduler.add_job(sendTweet, trigger=CronTrigger(hour=str(hourInt), minute=str(minuteInt)),id="worm_message")
+    print("Hours set to {0}:{1}".format(hourInt, minuteInt))
 
 scheduler = BackgroundScheduler()
 
 def runTwitter():
-    scheduler.start()
-
     print("Running Twitter!")
     client = tweepy.Client(twitterSecrets["bearerToken"],twitterSecrets["apiKey"], twitterSecrets["apiSecret"],twitterSecrets["accessToken"], twitterSecrets["accessSecret"])
 
@@ -43,9 +46,8 @@ def runTwitter():
         setRandTime(client, apiObj)
 
     scheduler.add_job(sendTweet, id="worm_message")
-
-    scheduler.add_job(setTime, trigger=CronTrigger(hour="00"), id="setTime")
-
+    scheduler.add_job(setTime, trigger=CronTrigger(hour="00"), id="set_time")
+    scheduler.start()
     # Run program infinitely
     while(True):
         sleep(1)
